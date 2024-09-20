638bff9b00c5a4ac3b83bb856b0eb7d89ef69a2b
Jake Zeisel <jacobzeisel@gmail.com>
2024-09-20 13:53:50 -0500

    update settings

--- main.c
+++ main.c
@@ -18,11 +18,17 @@
 Color PLOT_BLUE = SKYBLUE;
 Color PLOT_MAGENTA = (Color){.a = 255, .b = 150, .r = 150, .g = 75};
 Color PLOT_VIOLET = VIOLET;
-Color PLOT_GREEN = (Color){.a = 255, .b = 100, .r = 50, .g = 175};
-Color PLOT_YELLOW = (Color){.a = 255, .b = 50, .r = 175, .g = 150};
 
 float RMS = 0.0;
 double FFTG_BUFFER[FFT_LEN];
+
+typedef struct {
+    bool settings_open;
+    bool color_open;
+    int color_chosen;
+    float amplitude;
+    float latency;
+} Settings;
 
 double get_mag(double real, double imag) 
 {
@@ -57,11 +63,11 @@ void master_output_vu(int trackId,
         CSL_SR sample_rate,
         size_t num_channels) 
 {
-    float f_buffer[2000];
-    double d_buffer[2000];
-    double fftc_buffer[2000];
-    double fftr_buffer[2000];
-    int samps = byte_buffer_to_float_buffer(input_buffer, f_buffer, len, data_type);
+    float f_buffer[15000];
+    double d_buffer[15000];
+    double fftc_buffer[15000];
+    double fftr_buffer[15000];
+    int samps = byte_buffer_to_float_buffer(input_buffer, f_buffer, len, 15000, data_type);
     float new_rms = 0.0;
     for (int i = 0; i < samps; i++) 
     {
@@ -75,23 +81,23 @@ void master_output_vu(int trackId,
     {
         for (int i = 0; i < FFT_LEN; i++)
         {
-            fftr_buffer[i] = powf(get_log(get_mag(fftc_buffer[i*2], fftc_buffer[i*2 + 1])), 2);
+            fftr_buffer[i] = get_log(get_mag(fftc_buffer[i*2], fftc_buffer[i*2 + 1]));
         }
         memcpy(FFTG_BUFFER, fftr_buffer, FFT_LEN * sizeof(double));
     }
 }
 
-void start_sound(void)
+void start_sound(int microphone_latency)
 {
     int ret;
     ret = soundlib_start_session(SR48000, CSL_S32);
     int ind = soundlib_get_default_input_device_index();
 
-    ret = soundlib_start_input_stream(ind, 0.01);
+    ret = soundlib_start_input_stream(ind, microphone_latency);
 
     int oind = soundlib_get_default_output_device_index();
 
-    ret = soundlib_start_output_stream(oind, 0.01);
+    ret = soundlib_start_output_stream(oind, microphone_latency);
 
     soundlib_set_master_volume(-100.0);
 
@@ -101,22 +107,19 @@ void start_sound(void)
 
 int main(void)
 {
-    start_sound();
-    InitWindow(PLOT_WIDTH, PLOT_HEIGHT, "Audio Plot");
-
     float amp_values[MAX_VALUES] = {0}; // Array to store amplitude values
     double spec_values[MAX_VALUES][FFT_LEN] = {{0}, {0}}; // 2d array to store MAX_VAL number of FFT arrays
     int current_index = 0;           // Index for the latest value
     float time_elapsed = 0;          // Time elapsed for adding values
     int total_values = 0;            // Total number of values generated
-    bool show_settings = false;
-    int dropdownboxactive = 0;
-    bool active = false;
     Color default_background_color = SKYBLUE;
-
+    bool latency_changed = false;
+    Settings settings = { .color_chosen = 0, .color_open = false, .settings_open = false, .amplitude = 1500, .latency = 0.01};
     Font custom_font = LoadFont("./Inria_Sans/InriaSans-Regular.ttf");
-
     SetTargetFPS(60); 
+
+    start_sound(settings.latency);
+    InitWindow(PLOT_WIDTH, PLOT_HEIGHT, "Audio Plot");
     
     while (!WindowShouldClose())
     {
@@ -124,7 +127,7 @@ int main(void)
         if (time_elapsed > 0.001f) // Add a new value every N seconds
         {
             time_elapsed = 0;
-            amp_values[current_index] = 1500 * RMS; 
+            amp_values[current_index] = settings.amplitude * RMS; 
             memcpy(spec_values[current_index], FFTG_BUFFER, FFT_LEN);
 
             current_index = (current_index + 1) % MAX_VALUES; // Loop around if we reach the max
@@ -133,16 +136,18 @@ int main(void)
 
         if (IsKeyPressed(KEY_S))
         {
-            show_settings = !show_settings;
+            settings.settings_open = !settings.settings_open;
         }
-        switch(dropdownboxactive)
+        switch(settings.color_chosen)
         {
             case 0: default_background_color = PLOT_BLUE; break;
             case 1: default_background_color = PLOT_MAGENTA; break;
             case 2: default_background_color = PLOT_VIOLET; break;
-            case 3: default_background_color = PLOT_GREEN; break;
-            case 4: default_background_color = PLOT_YELLOW; break;
             default: default_background_color = SKYBLUE; 
+        }
+        if (latency_changed) 
+        {
+            latency_changed = false;
         }
 
         BeginDrawing();
@@ -173,7 +178,7 @@ int main(void)
         }
 
         /* draw settings */
-        if (show_settings == false) 
+        if (settings.settings_open == false) 
         {
             DrawTextEx(custom_font, "S = Settings", (Vector2){35, 35}, 22, 2, BLACK);
         }
@@ -182,10 +187,12 @@ int main(void)
             Color settings_color = (Color){.r = DARKGRAY.r, .g = DARKGRAY.g, .b = DARKGRAY.b, 150};
             DrawRectangle(35, 35, 400, PLOT_HEIGHT - 40 - 35, settings_color);
             DrawTextEx(custom_font, "Settings", (Vector2){125+65, 35+10}, 20, 2, BLACK);
-
-            if (GuiDropdownBox((Rectangle){35+35, 35+65, 150, 40}, "SKY BLUE;MAGENTA;VIOLET;GREEN;YELLOW", &dropdownboxactive, active))
+            DrawTextEx(custom_font, "Color", (Vector2){35 + 35, 35 + 75}, 17, 2, BLACK);
+            GuiSlider((Rectangle){35 + 35 + 150, 35 + 150, 150, 20}, "min", "max", &settings.amplitude, 0.0, 4000.0);
+            DrawTextEx(custom_font, "Amplitude", (Vector2){35 + 35, 35 + 150}, 17, 2, BLACK);
+            if (GuiDropdownBox((Rectangle){35+35 + 150, 35+65, 150, 40}, "SKY BLUE;MAGENTA;VIOLET", &settings.color_chosen, settings.color_open))
             {
-                active = !active;
+                settings.color_open = !settings.color_open;
             }
         }
         EndDrawing();
